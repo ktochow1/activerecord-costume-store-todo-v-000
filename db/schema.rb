ad094165e019bff436c736945b6b50370541b60a
ktochow1 <ktochow1@binghamton.edu>
2019-03-15 23:36:44 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 3) do
     t.float "price"
     t.boolean "family_friendly"
     t.string "opening_date"
-    t.string "closing_date"
+    t.text "closing_date"
     t.text "description"
   end
 
