ad020ee52f28188e8e55f01d4046933ad3d9eceb
ktochow1 <ktochow1@binghamton.edu>
2019-03-15 23:23:24 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -16,7 +16,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
       t.integer :price
       t.string :size
       t.string :image_url
-      t.timestamp
+      t.timestamp :created_at
+      t.timestamp :updated_at
     end
   end
   
