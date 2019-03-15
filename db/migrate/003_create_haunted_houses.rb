e7ea72d1541ba09bfd93772ee832250ac1cd82b4
ktochow1 <ktochow1@binghamton.edu>
2019-03-15 23:33:43 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -5,7 +5,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     create_table :haunted_houses do |t|
       t.string :name 
       t.string :location
-      t.string :theme
+      t.text :theme
       t.float :price
       t.boolean :family_friendly
       t.datetime :opening_time
