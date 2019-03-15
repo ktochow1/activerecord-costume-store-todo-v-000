ce56d4e59714f88a5bfc45343b3c281d1f719707
ktochow1 <ktochow1@binghamton.edu>
2019-03-15 23:14:47 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+  
+  def change 
+    create_table do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :in_business
+      t.float :opening_time
+      t.float :closing_time
+    end
+  end
+  
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+  
+  def change 
+    create_table do |t|
+      t.string :name 
+      t.string :location
+      t.float :price
+      t.boolean :family_friendly
+      t.float :opening_time
+      t.float :closing_time
+      t.string :long_description 
+    end
+  end
+end
