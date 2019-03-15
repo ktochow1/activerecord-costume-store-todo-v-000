0dc05df2965bea1f6f94e287b0769fcf76ca9604
ktochow1 <ktochow1@binghamton.edu>
2019-03-15 23:32:25 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,6 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
+      t.string :theme
       t.integer :costume_inventory
       t.integer :num_of_employees
       t.boolean :still_in_business
